ada1039d2f37baa597a36c295b8dd9ef7b777c45
Jacob Lavarine <Jlavarine6237@gmail.com>
2025-03-14 11:24:30 -0500

    fix: Update date key

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,3 @@
-export const getDateKey = () => {
-    const date = new Date();
-    date.setDate(date.getDate() - 6);
-    return date.toISOString().split('T')[0];
-  };
-  
+export function getDateKey() {
+    return new Date().toISOString().split('T')[0]
+}
